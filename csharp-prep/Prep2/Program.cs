9b080eab56fc9ad40ea12c10592fdb01f939d508
Dawson Truman <dawsontruman2525@gmail.com>
2025-09-18 13:49:04 -0600

    Completed Learning Activity - C# Prep 2 ...with all stretch challenges.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,72 @@
 using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // get grade percentage from user
+        Console.Write("Please enter your grade percentage: ");
+        float grade = float.Parse(Console.ReadLine());
+
+        // Determine letter grade
+        string letter;
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // Determine plus or minus
+        string plusMinus;
+        if (grade % 10 >= 7 & letter != "A" & letter != "F")
+        {
+            plusMinus = "+";
+        }
+        else if (grade % 10 < 3 & letter != "A" & letter != "F")
+        {
+            plusMinus = "-";
+        }
+        else
+        {
+            plusMinus = "";
+        }
+
+        // determine pass or fail
+        bool pass;
+        if (grade >= 70)
+        {
+            pass = true;
+        }
+        else
+        {
+            pass = false;
+        }
+
+        // display grade
+        Console.WriteLine($"Your grade: {letter}{plusMinus}");
+
+        // rub it in
+        if (pass)
+        {
+            Console.WriteLine("Congratulations, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass. Better luck next time!");
+        }
     }
 }
