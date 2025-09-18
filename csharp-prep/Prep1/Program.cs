a8626f990555bb32dfb57d80d3348c993716cca8
Dawson Truman <dawsontruman2525@gmail.com>
2025-09-18 12:46:03 -0600

    Completed Learning Activity - C# Prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("Please enter your first name: ");
+        string first = Console.ReadLine();
+        Console.Write("Please enter your last name: ");
+        string last = Console.ReadLine();
+        Console.Write($"\nYour name is {last}, {first} {last}.");
     }
 }
