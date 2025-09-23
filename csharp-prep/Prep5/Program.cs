099e2afe90fa28cfdfa766c0e6fcd3db2b107c9c
Dawson Truman <dawsontruman2525@gmail.com>
2025-09-23 17:36:15 -0600

    Learning Activity - C# Prep 5 Activity completed. Checked vs sample solution.

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,68 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int birthYear;
+        PromptUserBirthYear(out birthYear);
+        int squareNumber = SquareNumber(number);
+        DisplayResult(name, squareNumber, birthYear);
+    }
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+    static int PromptUserNumber()
+    {
+        int favNumber;
+        while (true)
+        {
+            Console.Write("Please enter your favorite number: ");
+            try
+            {
+                favNumber = int.Parse(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                continue;
+            }
+            break;
+        }
+        return favNumber;
+    }
+    static void PromptUserBirthYear(out int birthYear)
+    {
+        while (true)
+        {
+            Console.Write("Please enter the year you were born: ");
+            try
+            {
+                birthYear = int.Parse(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                continue;
+            }
+            break;
+        }
+    }
+    static int SquareNumber(int number)
+    {
+        return number * number;
+    }
+    static void DisplayResult(string name, int squareNumber, int birthYear)
+    {
+        Console.WriteLine($"{name}, the square of your number is {squareNumber}");
+        // calculate and display age of user, using 2025 as current year, 
+        // shamefully avoiding using DateTime for this assignment
+        int age = 2025 - birthYear;
+        Console.WriteLine($"{name}, you will turn {age} this year.");
     }
 }
