e6bdb937d3e6754aeb02d3135cc1f1dd11efc774
Dawson Truman <dawsontruman2525@gmail.com>
2025-09-23 14:59:40 -0600

    Learning Activity - C# Prep 4 Completed; didn't make much effort at the stretch challenges this time.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,57 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        // get a list of numbers from the user
+        List<double> numbers = new List<double>();
+
+        while (true)
+        {
+            // use a try-catch loop to catch common FormatException
+            double entry;
+            try
+            {
+                Console.Write("Please enter a number, or enter 0 when finished: ");
+                entry = double.Parse(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                continue;
+            }
+            // break if user entered a zero, otherwise add number to list and loop continues
+            if (entry == 0)
+            {
+                break;
+            }
+            else
+            {
+                numbers.Add(entry);
+            }
+        }
+
+        // compute the sum of the numbers in the list
+        double sum = 0;
+        foreach (double numb in numbers)
+        {
+            sum += numb;
+        }
+
+        // compute the mean average of the numbers in the list
+        // use sum from previous step
+        double average = sum / numbers.Count;
+
+        // find the maximum number in the list
+        double max = numbers[0];
+        foreach (double numb in numbers)
+        {
+            if (numb > max)
+            {
+                max = numb;
+            }
+        }
+
+        // display results
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The max is: {max}");
     }
 }
