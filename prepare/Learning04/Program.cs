c0ced61f032205bea1134e3d22c4f5bc2ede0fd2
Dawson Truman <dawsontruman2525@gmail.com>
2025-10-28 13:15:12 -0600

    Inheritance Learning Activity Completed activity.

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        // test Assignment
+        Assignment homework1 = new Assignment("Mary Jane", "Shave Three Tigers");
+        Console.WriteLine(homework1.GetSummary());
+
+        // test MathAssignment
+        MathAssignment mathHwk = new MathAssignment("Samuel Harrison", "Linear Algebra", "3.1", "3,5,9");
+        Console.WriteLine(mathHwk.GetSummary());
+        Console.WriteLine(mathHwk.GetHomeworkList());
+
+        // test WritingAssignment
+        WritingAssignment paper = new WritingAssignment("Dawson Truman", "CSE 210", "Articulate: Encapsulation");
+        Console.WriteLine(paper.GetSummary());
+        Console.WriteLine(paper.GetWritingInformation());   
     }
 }
